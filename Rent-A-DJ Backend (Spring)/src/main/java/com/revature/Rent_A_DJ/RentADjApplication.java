48ccc374344eaf6b631748e71616f05ab887a171
Garrett Kay <65757059+garrettkay@users.noreply.github.com>
2024-11-18 12:01:17 -0800

    manual upload, spring initializr template

---
+++ Rent-A-DJ Backend (Spring)/src/main/java/com/revature/Rent_A_DJ/RentADjApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Rent_A_DJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentADjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentADjApplication.class, args);
+	}
+
+}
---
+++ Rent-A-DJ Backend (Spring)/src/test/java/com/revature/Rent_A_DJ/RentADjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.Rent_A_DJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentADjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
