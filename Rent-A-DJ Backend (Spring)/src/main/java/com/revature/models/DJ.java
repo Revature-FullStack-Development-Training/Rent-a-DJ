23db7ddb0537949adc7420e1a31724f3b81d7538
garrettkay <garrettkay@gmail.com>
2024-11-20 10:20:53 -0800

    model default code

--- Rent-A-DJ Backend (Spring)/src/main/java/com/revature/models/DJ.java
+++ Rent-A-DJ Backend (Spring)/src/main/java/com/revature/models/DJ.java
@@ -1,4 +1,56 @@
 package com.revature.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Component //This Class will be registered as a Spring Bean
+@Entity //This Class will be created as a table in the DB (In other words, a DB ENTITY)
+@Table(schema="project2", name = "DJs") //@Table lets us set properties like table name. THIS IS NOT WHAT MAKES IT A TABLE
 public class DJ {
+
+    @Id //This makes the field the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //This makes our PK auto-increment (like serial)
+    private int DJId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "DJ", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonIgnore //prevents the circular reference in our JSON responses
+    private List<Reservation> reservations;
+
+    public DJ() {}
+
+    public DJ(int userid, String firstName, String lastName, String username, String password, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- Rent-A-DJ Backend (Spring)/src/main/java/com/revature/models/Reservation.java
+++ Rent-A-DJ Backend (Spring)/src/main/java/com/revature/models/Reservation.java
@@ -1,4 +1,61 @@
 package com.revature.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+@Setter
+@Getter
+@Component //This Class will be registered as a Spring Bean
+@Entity //This Class will be created as a table in the DB (In other words, a DB ENTITY)
+@Table(schema="project2", name = "reservations") //@Table lets us set properties like table name. THIS IS NOT WHAT MAKES IT A TABLE
 public class Reservation {
+
+    @Id //This makes the field the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //This makes our PK auto-increment (like serial)
+    private int reservationId;
+
+    @Column(nullable = false)
+    private LocalDateTime creationTime = LocalDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private LocalDateTime timedate = LocalDateTime.parse("2013-12-18T14:30");
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "DJId",referencedColumnName = "DJId") //this links our FK to the PK in User (has to be the same amount!!!)
+    private DJ dj;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId",referencedColumnName = "userId") //this links our FK to the PK in User (has to be the same amount!!!)
+    private User user;
+
+    public Reservation() {}
+
+    public Reservation(String location, String timedate, DJ dj, User user){
+        this.location = location;
+        this.timedate = LocalDateTime.parse(timedate);
+        this.dj = dj;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
