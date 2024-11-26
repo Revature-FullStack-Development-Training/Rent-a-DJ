6a5d4d2d33b25a4c438896ee791ea18d5eb9cb89
vishnusrinivasan <vishnusrnvsn@gmail.com>
2024-11-26 12:47:09 -0500

    trying to fix conflicts

--- Rent-A-DJ Backend (Spring)/src/main/java/com/revature/controllers/ReservationController.java
+++ Rent-A-DJ Backend (Spring)/src/main/java/com/revature/controllers/ReservationController.java
@@ -85,7 +85,6 @@ public class ReservationController {
         return ResponseEntity.ok(reservations);
     }
 
-<<<<<<< HEAD
     @PatchMapping("{reservationId}/status}")
     public ResponseEntity<Reservation> resolveReservation(@PathVariable int id, @RequestParam String status){
         Reservation reservation = reservationService.resolveReservation(id, status);
@@ -93,8 +92,7 @@ public class ReservationController {
         return ResponseEntity.ok(reservation);
     }
 
-    @PatchMapping("{reservationId}/location")
-=======
+
     //Handles changing reservation start time
     @PatchMapping("/{reservationId}/startTime")
     public ResponseEntity<Reservation> changeStartTime(@PathVariable int reservationId, @RequestBody LocalDateTime newTime){
@@ -104,7 +102,6 @@ public class ReservationController {
 
 
     @PatchMapping("/{reservationId}/location")
->>>>>>> 8f7a55b3a44044f8283a3e86bdd63742d477b6e4
     public ResponseEntity<Reservation> updateReservationLocation(@PathVariable int reservationId, @RequestBody String newLocation){
         Reservation reservation = reservationService.updateReservationLocation(reservationId, newLocation);
 
